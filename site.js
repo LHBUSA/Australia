6832f42e0e78ec6b3e6b0231e3b836f0ba71d40a
LHBUSA <sales@localhomebuyersusa.com>
2026-09-05 06:21:14 -0500

    Load light Australia workspace theme

--- site.js
+++ site.js
@@ -1,1 +1,10 @@
-(()=>{const load=(src,done)=>{const s=document.createElement('script');s.src=src;s.async=false;if(done)s.onload=done;document.head.appendChild(s)};load('/site.core.js',()=>load('/product-demo.js'))})();
+(()=>{
+  if(location.pathname.startsWith('/workspace')){
+    const l=document.createElement('link');
+    l.rel='stylesheet';
+    l.href='/workspace-light.css';
+    document.head.appendChild(l);
+  }
+  const load=(src,done)=>{const s=document.createElement('script');s.src=src;s.async=false;if(done)s.onload=done;document.head.appendChild(s)};
+  load('/site.core.js',()=>load('/product-demo.js'));
+})();
